806b0b387463ebfe69a6bdc3d17b000471f5cbf0
Himanshu Kumar Vaidya <76218691+vaidyahimanshu502@users.noreply.github.com>
2023-06-03 23:10:40 +0530

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const userController = require('../controllers/user_controller');
 
 // for USER
-    // router.post('/create', userController.register_user);
-    // router.post('/sign-in', userController.sign_in_user);
+    router.post('/create', userController.register_user);
+    router.post('/sign-in', userController.sign_in_user);
 
 // for ADMIN
-router.post('/create', userController.signup);
-router.post('/log-in', userController.login);
+//router.post('/create', userController.signup);
+//router.post('/log-in', userController.login);
 
 module.exports=router;
